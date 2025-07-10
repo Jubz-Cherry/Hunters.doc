1c5936fc035228584afe8c9e4baef682eaa856f9
GabrielMartinJs <gabrieluzumaki2006@gmail.com>
2025-07-09 21:24:43 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <h2>oioi</h2>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
